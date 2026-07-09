a792e4a72f27b516a27079688eb2ad976eb550c2
Csushobhit <csushobhit5515@gmail.com>
2026-07-09 21:45:37 +0530

    initial commit

---
+++ distributed-task-queue/src/main/java/com/sushobhit/taskqueue/App.java
@@ -0,0 +1,10 @@
+package com.sushobhit.taskqueue;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
